374fc1caeb657573761f5d600f51831bd7ae337a
Administrator <administrator@Kolas-MBP.hitronhub.home>
2020-05-12 16:22:24 -0600

    with initial setup files

---
+++ lib/screens/placesList.screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './addPlace.screen.dart';
+import '../providers/greatPlaces.provider.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          )
+        ],
+      ),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: Text('Start adding some places!'),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+            ? ch
+            : ListView.builder(
+                itemCount: greatPlaces.items.length,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: FileImage(greatPlaces.items[i].image),
+                  ),
+                  title: Text(greatPlaces.items[i].title),
+                  onTap: (){},
+                ),
+              ),
+      ),
+    );
+  }
+}
